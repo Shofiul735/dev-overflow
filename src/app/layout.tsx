347aaff7bb50cdd4fdc0f1cb471e1a1ad53b5cc3
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-11-02 22:38:17 +0600

    font issue fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,26 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const inter = localFont({
-  src: "./font/Inter/Inter.tff",
+  src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
-  weight: "100 200 300 400 500 600 700 800 900",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
 const spaceGrotesk = localFont({
-  src: "./font/Space_Grotesk/SpaceGrotesk-VF.tff",
+  src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
-  weight: "300 400 500 600 700",
+  weight: "300 400 500 700",
 });
+
+export const metadata: Metadata = {
+  title: "Dev Overflow",
+  description: `A community-driven platform for asking and answering programming questions. Get help, share knowledge, 
+    and collaborate with developers from around the world. Explore topics in web development, mobile app development, 
+    algorithms, data structures, and more.`,
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
