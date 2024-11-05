db2dd769d98493c05f6abbc98995644c8a7ecce8
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-11-05 13:21:40 +0600

    theme provider added in the project

---
+++ src/context/Theme.tsx
@@ -0,0 +1,8 @@
+"use clinet";
+
+import { ThemeProviderProps } from "next-themes";
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+
+export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+};
